f9381e0ea034a7b689c6a2ab00caab16303480bb
shahidnasim <snasim1786@gmail.com>
2023-08-21 13:38:39 +0530

    forgotPassworesds

--- index.js
+++ index.js
@@ -139,6 +139,24 @@ app.post("/forgotPassword",async(req,res)=>{
     }
   })
 
+  // optMatch
+
+  app.post("/otpMatch/:id",async(req,res)=>{
+const {otp}=req.body
+console.log('otpnew',otp)
+// console.log(req.body)
+const userId=req.params.id
+// console.log('eq.param',req.params.id)
+const isExistEmail=await register.findOne({_id:userId})
+console.log("otp",isExistEmail);
+
+if(isExistEmail?.otp==otp){
+res.send({message:"otp Match"})
+  } else{
+    res.send({message:"otp Not Match"})
+  }
+}
+)
 
 
 
